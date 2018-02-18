d53daf30bd7b22a44b4b6723aeec27fd443e733a
justumen <me@justumen.org>
2018-02-18 18:36:32 +0100

    bash 4

--- gamescript.sh
+++ gamescript.sh
@@ -10,13 +10,13 @@ command -v base64 >/dev/null 2>&1 || { echo "The command base64 is needed to con
 
 function answer_quizCOPY_bash(){
 	key="9"
-	while [ "$key" != "1" ] || [ "$key" != "2" ] || [ "$key" != "3" ] || [ "$key" != "4" ]; do
+	while [ "$key" != "1" ] || [ "$key" != "2" ] || [ "$key" != "3" ] || [ "$key" != "4" ] || [ "$key" != "5" ]; do
 		echo ""
 		echo -e "\\e[0;100m 1) \\e[0m $1"
 		echo -e "\\e[0;100m 2) \\e[0m $2"
 		echo -e "\\e[0;100m 3) \\e[0m $3"
 		echo -e "\\e[0;100m 4) \\e[0m $4"
-		echo -e "\\e[0;100m 4) \\e[0m $5"
+		echo -e "\\e[0;100m 5) \\e[0m $5"
 		echo -en "\\e[1;31;42m # \\e[0m"
 		read -n1 key < /dev/tty
 		case $key in
