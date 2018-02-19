df43c4fe5add953fcd9370f33845b0261216a359
justumen <me@justumen.org>
2018-02-19 19:10:10 +0100

    bash 4

--- fr/classic/bash/_5/QUIZ_4GEN.sh
+++ fr/classic/bash/_5/QUIZ_4GEN.sh
@@ -4,15 +4,15 @@ function quiz(){
   talk_not_press_key justumen "- La réponse doit être la plus courte possible, une commande valide mais ajoutant des caractères inutiles ne fonctionnera pas."
   talk_not_press_key justumen "Exemple : si la réponse est 'ls'. Les réponses 'ls .', 'ls ./' et 'ls ././' seront considérées comme fausses."
   answer_text_fr "Comment afficher les permissions des éléments du répertoire de travail de l'utilisateur ?" "ls -l ~"
-  answer_text_fr "Quel lettre représente un dossier dans le résultat de 'ls -l' ?" "d"
+  answer_text_fr "Quelle lettre représente un dossier dans le résultat de 'ls -l' ?" "d"
   answer_text_fr "Quel symbole représente un fichier dans le résultat de 'ls -l' ?" "-"
-  answer_text_fr "Quel lettre représente le droit d'écriture dans le résultat de 'ls -l' ?" "w"
-  answer_text_fr "Quel lettre représente le droit de lecture dans le résultat de 'ls -l' ?" "r"
-  answer_text_fr "Quel lettre représente le droit d'exécution dans le résultat de 'ls -l' ?" "x"
-  answer_text_fr "Quel est la commande pour modifier les permissions d'un fichier ?" "chmod"
-  answer_text_fr "Quel lettre représente le propriétaire pour la commande chmod ?" "u"
+  answer_text_fr "Quelle lettre représente le droit d'écriture dans le résultat de 'ls -l' ?" "w"
+  answer_text_fr "Quelle lettre représente le droit de lecture dans le résultat de 'ls -l' ?" "r"
+  answer_text_fr "Quelle lettre représente le droit d'exécution dans le résultat de 'ls -l' ?" "x"
+  answer_text_fr "Quelle est la commande pour modifier les permissions d'un fichier ?" "chmod"
+  answer_text_fr "Quelle lettre représente le propriétaire pour la commande chmod ?" "u"
   answer_text_fr "Comment supprimer la permission de lecture au propriétaire du fichier 'test' ?" "chmod u-r test"
-  answer_text_fr "Comment ajouter la permission d'exécution à tous les utilisateurs sauf pour le propriétaire sur le fichier 'test' ?" "chmod go+x test"
+  answer_text_fr "Comment ajouter la permission d'exécution sur le fichier 'test' à tous les utilisateurs sauf pour le propriétaire ?" "chmod go+x test"
   unlock
 }
 
