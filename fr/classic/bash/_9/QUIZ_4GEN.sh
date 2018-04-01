c318c9247107cf92c7e198baa4af61d42a20197a
justumen <me@justumen.org>
2018-04-01 09:59:53 +0200

    bash 9

--- fr/classic/bash/_9/QUIZ_4GEN.sh
+++ fr/classic/bash/_9/QUIZ_4GEN.sh
@@ -13,9 +13,9 @@ function start_quiz(){
   echo -e "\e[15;5;44m Bash 'Bourne Again SHell' : Questionnaire du chapitre 9 \e[0m"
   echo -e "- La réponse doit être la plus courte possible, une commande valide mais ajoutant des caractères inutiles ne fonctionnera pas."
   echo -e "Exemple : si la réponse est 'ls'. Les réponses 'ls .', 'ls ./' et 'ls ././' seront considérées comme fausses."
-  answer_text_fr "Comment afficher le contenu de la variable 'PATH' ?" "echo $PATH"
-  answer_text_fr "Comment ajouter ':/bin' à la fin de la variable 'PATH' ?" "PATH=$PATH:/bin"
-  answer_text_fr "Sans utiliser les '\"', comment ajouter un espace à la fin de la variable 'phrase' ?" "phrase=$phrase' '"
+  answer_text_fr "Comment afficher le contenu de la variable 'PATH' ?" 'echo $PATH'
+  answer_text_fr "Comment ajouter ':/bin' à la fin de la variable 'PATH' ?" 'PATH=$PATH:/bin'
+  answer_text_fr "Sans utiliser les apostrophes, comment ajouter un espace à la fin de la variable 'phrase' ?" 'phrase=$phrase" "'
   answer_text_fr "Comment afficher vos variables d'environnement avec 'less' ?" "printenv|less"
   answer_text_fr "Comment afficher le chemin absolu du fichier utilisé par la commande 'date' ?" "type date"
   answer_text_fr "Si vous executez le script bash '/scripts/sc' dans le dossier '/var/' et que ce script contient le code 'rm f', quel est le chemin absolu du fichier que 'bash' voudra supprimer ?" "/var/f"
