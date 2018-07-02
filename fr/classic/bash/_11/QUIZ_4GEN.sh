4688fb80a80ac7cf9243e20a426996d3ef53b2ae
justumen <me@justumen.org>
2018-07-02 20:06:44 +0200

    bash 11

--- fr/classic/bash/_11/QUIZ_4GEN.sh
+++ fr/classic/bash/_11/QUIZ_4GEN.sh
@@ -18,8 +18,13 @@ function start_quiz(){
   echo -e "\e[15;5;44m Bash 'Bourne Again SHell' : Questionnaire du chapitre 11 \e[0m"
   echo -e "- La réponse doit être la plus courte possible, une commande valide mais ajoutant des caractères inutiles ne fonctionnera pas."
   echo -e "Exemple : si la réponse est 'ls'. Les réponses 'ls .', 'ls ./' et 'ls ././' seront considérées comme fausses."
-  exit
-  unlock "bash" "10" "2211" "ddfb"
+  answer_text_fr "Par quel mot clef doit se terminer une condition 'if' ?" "fi"
+  answer_text_fr "Quel argument pouvez-vous donner à 'if' qui garantira l'execution de son 'else' ?" "false"
+  answer_text_fr "Comment demander la valeur de la variable 'nom' à l'utilisateur ?" "read nom"
+  answer_text_fr "En utilisant 'if', un seul '[' et un seul ']', comment afficher le mot 'oui' si la valeur de X est plus grande que 2 ?" "if [ $X -gt 2 ];then echo oui;fi"
+  answer_text_fr "Sans utiliser de 'if' et en utilisant qu'un seul '[' et qu'un seul ']', comment afficher le mot 'oui' si la valeur de X est plus petite que 2 ?" "[ $X -lt 2 ]&&echo oui"
+  answer_text_fr "Sans utiliser de 'if' ou de '[', comment afficher le mot 'oui' si la valeur de X est égal à 2 ?" "test $X -eq 2&&echo oui"
+  unlock "bash" "11" "2211" "ddfb"
 }
 
 
