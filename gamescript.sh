bbd1159455ddcb3b72819d1901943e465e3a19e1
justumen <me@justumen.org>
2018-09-29 15:09:17 +0700

    ../..

--- gamescript.sh
+++ gamescript.sh
@@ -195,8 +195,11 @@ while [ true ]; do
 		echo -e "\\e[0;100m e) \\e[0m Exit"
 		;;
   esac
+  selected="x"
+  while [ "$selected" != "1" ] && [ "$selected" != "2" ] && [ "$selected" != "3" ] && [ "$selected" != "h" ] && [ "$selected" != "p" ] && [ "$selected" != "e" ]; do
+	echo -en "\e[97;45m # \e[0m"
 	read selected < /dev/tty
-
+  done
   case $selected in
     "1") launch_gamescript $LANGUAGE classic bash ;;
     "2") launch_gamescript $LANGUAGE classic i3wm ;;
@@ -389,6 +392,10 @@ function create_config(){
 	echo -e "\\e[0;100m e) \\e[0m exit"
 	echo -en "\e[97;45m # \e[0m"
 	read default_language < /dev/tty
+    while [ "$selected" != "1" ] && [ "$selected" != "2" ] && [ "$selected" != "e" ]; do
+    	echo -en "\e[97;45m # \e[0m"
+		read default_language < /dev/tty
+    done
 
 	case $default_language in
 		1) LANGUAGE="en" ;; 
