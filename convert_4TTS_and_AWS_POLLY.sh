34ec66994979617d253c225f1598ee5d62c625d7
umen <u-men@outlook.com>
2020-03-11 07:36:18 +0100

    ../..

--- convert_4TTS_and_AWS_POLLY.sh
+++ convert_4TTS_and_AWS_POLLY.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# THIS CONVERT IN ENGLISH ONY FOR NOW
+# 1 - THIS CONVERT IN ENGLISH ONLY FOR NOW
+# ??? MAKE IT WITH ARGUMENTS ???
 line_number=1
 while read LINE; do
 	echo "line_number = $line_number"
@@ -8,8 +9,9 @@ while read LINE; do
 		QUIZ=`echo "$LINE" | grep "^!"`
 		if [[ "$QUIZ" == "" ]]; then
 			QUESTION=`echo "$LINE" | grep "^+"`
+			
 			if [[ "$QUESTION" == "" ]]; then
-				TTS_LINE=`echo $LINE | sed 's#/# SLASH #g' | sed 's#\${learn}##g' | sed 's#\${voc}##g' | sed 's#\${reset}##g' | sed 's#^+##g'`
+				TTS_LINE=`echo $LINE | sed 's#/# SLASH #g' | sed 's#\${learn}##g' | sed 's#\${voc}##g' | sed 's#\${reset}##g' | sed 's#\${codeFile}##g' | sed 's#\${codeError}##g' | sed 's#\${code}##g' | sed 's#^+##g'`
 				echo " ===> $TTS_LINE"
 				if [ ! -f "AWS_POLLY/$line_number.mp3" ]; then
 					#AWS POLLY NEEDS TO BE CONFIGURED OFC
